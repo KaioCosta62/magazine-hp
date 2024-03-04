198d25a27fa1627d217546d9e93dae6e3223df6a
KaioCosta62 <kaiohenriqueranking1@gmail.com>
2024-03-04 15:53:34 -0300

    criando a logica de abrir e fechar o carrinho

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 import { renderizarProdutosNaTela } from "./src/cartaoProduto.js";
+import { inicializarOCarrinho } from "./src/menuCarrinho.js";
 
 renderizarProdutosNaTela()
+inicializarOCarrinho()
---
+++ src/menuCarrinho.js
@@ -0,0 +1,23 @@
+const buttonsCarrinho = {
+    btnAbrirCarrinho:  document.getElementById('abrir-carrinho'),
+    btnFecharCarrinho: document.getElementById('fechar-carrinho')
+}
+
+const containerCarrinho = document.getElementById('carrinho')
+
+function abrirCarrinho(){
+    containerCarrinho.classList.remove('right-[-360px]')
+    containerCarrinho.classList.add('right-[0]')
+
+}
+
+function fecharCarrinho(){
+    containerCarrinho.classList.remove('right-[0]')
+    containerCarrinho.classList.add('right-[-360px]')
+}
+
+export function inicializarOCarrinho(){
+    const {btnAbrirCarrinho, btnFecharCarrinho} = buttonsCarrinho
+    btnAbrirCarrinho.addEventListener('click', abrirCarrinho)
+    btnFecharCarrinho.addEventListener('click', fecharCarrinho)
+}
