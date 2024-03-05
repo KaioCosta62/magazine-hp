12e452d75ed592ffe007d8da070629e528989920
KaioCosta62 <kaiohenriqueranking1@gmail.com>
2024-03-05 11:15:09 -0300

    criando funcionalidade de alterar quantidade de itens no carrinho

--- src/menuCarrinho.js
+++ src/menuCarrinho.js
@@ -1,5 +1,8 @@
 import { catalogoProdutos } from "./catalogoDeProdutos.js"
 
+const idsProdutoCarrinhoComQuantidade = {}
+
+
 const buttonsCarrinho = {
     btnAbrirCarrinho:  document.getElementById('abrir-carrinho'),
     btnFecharCarrinho: document.getElementById('fechar-carrinho')
@@ -17,7 +20,28 @@ function fecharCarrinho(){
     containerCarrinho.classList.add('right-[-360px]')
 }
 
+function incrementarQuantidadeProdutoNoCarrinho(idProduto){
+    idsProdutoCarrinhoComQuantidade[idProduto]++
+    console.log(idsProdutoCarrinhoComQuantidade)
+    atualizarInformacoesQuantidade(idProduto)
+}
+
+function decrementarQuantidadeProdutoNoCarrinho(idProduto){
+    idsProdutoCarrinhoComQuantidade[idProduto]--
+    atualizarInformacoesQuantidade(idProduto)
+}
+
+function atualizarInformacoesQuantidade(idProduto){
+    document.getElementById(`quantidade-${idProduto}`).innerText = idsProdutoCarrinhoComQuantidade[idProduto]
+}
+
 export function desenharProdutoNoCarrinho(idProduto){
+    if(idProduto in idsProdutoCarrinhoComQuantidade){
+        incrementarQuantidadeProdutoNoCarrinho(idProduto)
+    }
+
+    idsProdutoCarrinhoComQuantidade[idProduto] = 1
+    
     const produto = catalogoProdutos.find((produto) => produto.id === idProduto)
     const containerProdutoCarrinho = document.getElementById('produtos-carrinho')
 
@@ -28,15 +52,27 @@ export function desenharProdutoNoCarrinho(idProduto){
             <i class="fa-solid fa-xmark"></i>
             </button>
             <div class="p-2 flex flex-col justify-between gap-2">
-            <p class="text-slate-200 text-sm">Zara</p>
-            <p class="text-slate-200 text-sm">Camisa Larga com Bolsos</p>
-            <p class="text-lg text-green-700 font-bold">R$ 70,00</p>
+                <p class="text-slate-200 text-sm">Zara</p>
+                <p class="text-slate-200 text-sm">Camisa Larga com Bolsos</p>
+                <p class="text-lg text-green-700 font-bold">R$ 70,00</p>
+            </div>
+            <div class='absolute right-2 bottom-0 text-lg text-white flex gap-2'>
+                <button id='incrementar-produto-${produto.id}'>
+                    +
+                </button>
+                <p id='quantidade-${produto.id}'> ${idsProdutoCarrinhoComQuantidade[produto.id]} </p>
+                <button id='decrementar-produto-${produto.id}'>
+                   -
+                </button>
             </div>
         </article>
     `
 
     containerProdutoCarrinho.innerHTML += htmlProdutoCarrinho
-
+    
+    document.getElementById(`incrementar-produto-${produto.id}`).addEventListener('click', () => incrementarQuantidadeProdutoNoCarrinho(produto.id))
+    
+    document.getElementById(`decrementar-produto-${produto.id}`).addEventListener('click', () => decrementarQuantidadeProdutoNoCarrinho(produto.id))
 }
 
 
